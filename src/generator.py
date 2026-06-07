87ece852bf49e1ffe5407e563138c5ffbacb17cf
Max Shablin <maksimchick@mail.ru>
2026-06-08 02:36:44 +0300

    add calculate metrics function

--- src/generator.py
+++ src/generator.py
@@ -3,6 +3,37 @@ from render import MarkdownReadmeBuilder
 from src import config
 
 
+def calculate_metrics(raw_count: int, valid_data: list[dict]) -> ProxyMetrics:
+    alive_count = len(valid_data)
+    dead_count = raw_count - alive_count
+
+    avg_latency = (
+        round(
+            sum(
+                p["latency_ms"]
+                for p in valid_data
+            ) / alive_count, 2
+        )
+        if alive_count
+        else 0
+    )
+
+    rate = (
+        round((alive_count / raw_count * 100), 1)
+        if raw_count
+        else 0.0
+    )
+
+    return ProxyMetrics(
+        total=raw_count,
+        valid=valid_data,
+        alive_count=alive_count,
+        dead_count=dead_count,
+        avg_latency=avg_latency,
+        rate=rate
+    )
+
+
 def generate_readme(stats: ProxyMetrics):
     readme = (
         MarkdownReadmeBuilder()
